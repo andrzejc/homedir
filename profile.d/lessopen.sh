caaebe5d595dec5d71ba5d55c1db57e4ba5e4b80
Andrzej Ciarkowski <ciarkoa@amazon.com>
2017-10-06 22:19:37 +0200

    push

--- profile.d/lessopen.sh
+++ profile.d/lessopen.sh
@@ -1,10 +1,16 @@
+# setup less input/output preprocessor (LESSOPEN) env var
+
 SRCHILITE_SH="$(which src-hilite-lesspipe.sh 2>/dev/null)"
 LESSPIPE_SH="$(which lesspipe.sh 2>/dev/null)"
 
-if [ -x "$SRCHILITE_SH" ]
-then 
-	export LESSOPEN="| $SRCHILITE_SH %s"
-elif [ -x "$LESSPIPE_SH" ]
+
+if [ -x "$LESSPIPE_SH" ]
 then
 	export LESSOPEN="| $LESSPIPE_SH %s"
+	# src-hilite-lesspipe.sh requires regular lesspipe
+	if [ -x "$SRCHILITE_SH" ]
+	then
+		alias lesspipe="$LESSPIPE_SH"
+		eval "$( "$SRCHILITE_SH" )"
+	fi
 fi
