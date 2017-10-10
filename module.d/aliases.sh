f0376854f4955b3511aa228e81de93b27c94f813
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2017-10-10 17:20:41 +0200

    push
    Change-Id: Ic848e7cd0582aaeb979442cb8d0411eb7bf715c0

--- module.d/aliases.sh
+++ module.d/aliases.sh
@@ -1,7 +1,16 @@
 # start tmux in 256-color mode
 [[ $TERM == *256col* ]] && alias tmux="tmux -2"
 
+alias la='ls -A'
+alias l='ls -CF'
 alias ll='ls -lAh'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
 cd() { builtin cd "$@"; ll; }
 
