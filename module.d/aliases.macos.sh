d96ac73505b68d4f5514fa8ee711664df3254b6e
Andrzej Ciarkowski <ciarkoa@amazon.com>
2017-10-23 23:50:36 +0200

    push

--- module.d/aliases.macos.sh
+++ module.d/aliases.macos.sh
@@ -1,3 +1,4 @@
+source "${HOMEDIR}/module.d/aliases.sh"
 
 alias f='open -a Finder ./'                 # f:            Opens current directory in MacOS Finder
 #alias ~="cd ~"                              # ~:            Go Home
@@ -9,7 +10,7 @@ alias fix_stty='stty sane'                  # fix_stty:     Restore terminal set
 trash () { command mv "$@" ~/.Trash ; }     # trash:        Moves a file to the MacOS trash
 ql () { qlmanage -p "$*" >& /dev/null; }    # ql:           Opens any file in MacOS Quicklook Preview
 #alias DT='tee ~/Desktop/terminalOut.txt'    # DT:           Pipe content to file on MacOS Desktop
- 
+
 #   mans:   Search manpage given in agument '1' for term given in argument '2' (case insensitive)
 #           displays paginated result with colored search terms and two lines surrounding each hit.             Example: mans mplayer codec
 #   --------------------------------------------------------------------
@@ -20,7 +21,7 @@ mans () {
 #   -------------------------------
 #   3.  FILE AND FOLDER MANAGEMENT
 #   -------------------------------
- 
+
 #   cdf:  'Cd's to frontmost window of MacOS Finder
 #   ------------------------------------------------------
 cdf () {
@@ -42,12 +43,12 @@ cdf () {
 #   spotlight: Search for a file using MacOS Spotlight's metadata
 #   -----------------------------------------------------------
 spotlight () { mdfind "kMDItemDisplayName == '$@'wc"; }
- 
- 
+
+
 #   ---------------------------
 #   5.  PROCESS MANAGEMENT
 #   ---------------------------
- 
+
 #   findPid: find out the pid of a specified process
 #   -----------------------------------------------------
 #       Note that the command name can be specified via a regex
@@ -55,36 +56,36 @@ spotlight () { mdfind "kMDItemDisplayName == '$@'wc"; }
 #       Without the 'sudo' it will only find processes of the current user
 #   -----------------------------------------------------
 findpid () { lsof -t -c "$@" ; }
- 
+
 #   memHogsTop, memHogsPs:  Find memory hogs
 #   -----------------------------------------------------
 alias mtop='top -l 1 -o rsize | head -20'
 alias mhogs='ps wwaxm -o pid,stat,vsize,rss,time,command | head -10'
- 
+
 #   cpuHogs:  Find CPU hogs
 #   -----------------------------------------------------
 alias chogs='ps wwaxr -o pid,stat,%cpu,time,command | head -10'
- 
+
 #   topForever:  Continual 'top' listing (every 10 seconds)
 #   -----------------------------------------------------
 alias ctop='top -l 9999999 -s 10 -o cpu'
- 
+
 #   ttop:  Recommended 'top' invocation to minimize resources
 #   ------------------------------------------------------------
 #       Taken from this macosxhints article
 #       http://www.macosxhints.com/article.php?story=20060816123853639
 #   ------------------------------------------------------------
 alias ttop="top -R -F -s 10 -o rsize"
- 
+
 #   my_ps: List processes owned by my user:
 #   ------------------------------------------------------------
 my_ps() { ps $@ -u $USER -o pid,%cpu,%mem,start,time,bsdtime,command ; }
- 
- 
+
+
 #   ---------------------------
 #   6.  NETWORKING
 #   ---------------------------
- 
+
 alias myip='curl ip.appspot.com'                    # myip:         Public facing IP Address
 alias netCons='lsof -i'                             # netCons:      Show all open TCP/IP sockets
 alias flushDNS='dscacheutil -flushcache'            # flushDNS:     Flush out the DNS Cache
@@ -95,7 +96,7 @@ alias ipInfo0='ipconfig getpacket en0'              # ipInfo0:      Get info on 
 alias ipInfo1='ipconfig getpacket en1'              # ipInfo1:      Get info on connections for en1
 alias openPorts='sudo lsof -i | grep LISTEN'        # openPorts:    All listening connections
 alias showBlocked='sudo ipfw list'                  # showBlocked:  All ipfw rules inc/ blocked IPs
- 
+
 #   ii:  display useful host related informaton
 #   -------------------------------------------------------------------
 ii() {
@@ -113,13 +114,13 @@ ii() {
 #   ---------------------------------------
 #   7.  SYSTEMS OPERATIONS & INFORMATION
 #   ---------------------------------------
- 
+
 #alias mountReadWrite='/sbin/mount -uw /'    # mountReadWrite:   For use when booted into single-user
- 
+
 #   cleanupDS:  Recursively delete .DS_Store files
 #   -------------------------------------------------------------------
 alias cleanupDS="find . -type f -name '*.DS_Store' -ls -delete"
- 
+
 #   finderShowHidden:   Show hidden files in Finder
 #   finderHideHidden:   Hide hidden files in Finder
 #   -------------------------------------------------------------------
@@ -129,34 +130,34 @@ alias finderHideHidden='defaults write com.apple.finder ShowAllFiles FALSE'
 #   cleanupLS:  Clean up LaunchServices to remove duplicates in the "Open With" menu
 #   -----------------------------------------------------------------------------------
 alias cleanupLS="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
- 
+
 #    screensaverDesktop: Run a screensaver on the Desktop
 #   -----------------------------------------------------------------------------------
 alias screensaverDesktop='/System/Library/Frameworks/ScreenSaver.framework/Resources/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine -background'
- 
+
 #   ---------------------------------------
 #   9.  REMINDERS & NOTES
 #   ---------------------------------------
- 
+
 #   remove_disk: spin down unneeded disk
 #   ---------------------------------------
 #   diskutil eject /dev/disk1s3
- 
+
 #   to change the password on an encrypted disk image:
 #   ---------------------------------------
 #   hdiutil chpass /path/to/the/diskimage
- 
+
 #   to mount a read-only disk image as read-write:
 #   ---------------------------------------
 #   hdiutil attach example.dmg -shadow /tmp/example.shadow -noverify
- 
+
 #   mounting a removable drive (of type msdos or hfs)
 #   ---------------------------------------
 #   mkdir /Volumes/Foo
 #   ls /dev/disk*   to find out the device to use in the mount command)
 #   mount -t msdos /dev/disk1s1 /Volumes/Foo
 #   mount -t hfs /dev/disk1s1 /Volumes/Foo
- 
+
 #   to create a file of a given size: /usr/sbin/mkfile or /usr/bin/hdiutil
 #   ---------------------------------------
 #   e.g.: mkfile 10m 10MB.dat
